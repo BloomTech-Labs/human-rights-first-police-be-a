9adef544c335919fc4df845bc9b9d10a77b17de8
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-02-17 22:22:50 -0800

    Created [GET] Endpoints to get: all pending, approved and rejected twitter-incidents

--- api/twitter_incidents/twitterIncidentsModel.js
+++ api/twitter_incidents/twitterIncidentsModel.js
@@ -2,18 +2,35 @@ const db = require('../../data/db-config');
 
 module.exports = {
   getAllPendingIncidents,
+  getAllRejectedIncidents,
+  getAllApprovedIncidents,
   getTwitterIncidentById,
 };
 
 /**
- * Returns all incidents in the db sorted by newest incident first
+ * Returns all pending Twitter incidents in the db sorted by newest incident first
  */
 function getAllPendingIncidents() {
   return db('twitter_incidents')
     .where({ pending: true })
     .orderBy('date', 'desc');
 }
-
+/**
+ * Returns all rejected Twitter incidents in the db sorted by newest incident first
+ */
+function getAllRejectedIncidents() {
+  return db('twitter_incidents')
+    .where({ rejected: true })
+    .orderBy('date', 'desc');
+}
+/**
+ * Returns all approved Twitter incidents in the db sorted by newest incident first
+ */
+function getAllApprovedIncidents() {
+  return db('twitter_incidents')
+    .where({ approved: true })
+    .orderBy('date', 'desc');
+}
 /**
  * @param {string} id
  * Function to return a specific incident by provided id
--- api/twitter_incidents/twitterIncidentsRouter.js
+++ api/twitter_incidents/twitterIncidentsRouter.js
@@ -12,4 +12,21 @@ router.get('/incidents', async (req, res) => {
   }
 });
 
+router.get('/incidents/rejected', async (req, res) => {
+  try {
+    const twitterRejectedAdminFeed = await twitterIncidentHelper.getAllRejectedIncidents();
+    res.status(200).json(twitterRejectedAdminFeed);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+router.get('/incidents/approved', async (req, res) => {
+  try {
+    const twitterApprovedAdminFeed = await twitterIncidentHelper.getAllApprovedIncidents();
+    res.status(200).json(twitterApprovedAdminFeed);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
