f87cdf04a7992c681e5e24a9b307cc7b4699bc2a
Wesley-Ryan <im.wesleyryan@gmail.com>
2021-02-25 19:26:48 -0800

    Restructured Reddit table

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -2,22 +2,19 @@ exports.up = function (knex) {
   return knex.schema
     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('incidents', (incidents) => {
-      incidents.string('incident_id').unique().primary().notNullable();
+      incidents.integer('id').unique().primary().notNullable();
+      incidents.date('date');
+      incidents.date('added_on');
       incidents.text('src');
-      incidents.string('categories');
+      incidents.string('incident_id');
       incidents.string('city').notNullable();
       incidents.string('state').notNullable();
       incidents.float('lat').notNullable();
       incidents.float('long').notNullable();
       incidents.string('title').notNullable();
       incidents.varchar('desc', 10000);
-      incidents.date('date');
-      incidents.boolean('verbalization').defaultsTo(0);
-      incidents.boolean('empty_hand_soft').defaultsTo(0);
-      incidents.boolean('empty_hand_hard').defaultsTo(0);
-      incidents.boolean('less_lethal_methods').defaultsTo(0);
-      incidents.boolean('lethal_force').defaultsTo(0);
-      incidents.boolean('uncategorized').defaultsTo(0);
+      incidents.string('categories');
+      incidents.string('force_rank');
     })
     .createTable('twitter_incidents', (twitter_incidents) => {
       twitter_incidents.increments('server_id');
