38ccb6fd61debe8c73b2a084ef943e1d5be127d2
Michael Maton <mjmaton6@gmail.com>
2021-03-30 12:05:48 -0500

    updated knexfile for heroku migration

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,10 +1,49 @@
+// const dotenv = require('dotenv');
+// dotenv.config({ path: '../.env' });
+// const pg = require('pg')
+
+// const localConnection = process.env.localConnectionpwd
+// //const localConnection = 'postgresql://localhost/deployedHRF'
+
+// if (process.env.DATABASE_URL) {
+//   pg.defaults.ssl = { rejectUnauthorized: false }
+//   connection = process.env.DATABASE_URL;
+// } else {
+//   connection = localConnection
+// }
+
+// const sharedConfig = {
+//   client: 'pg',
+//   connection,
+//   migrations: { directory: "../data/migrations" },
+//   seeds: { directory: "../data/seeds" },
+// };
+
+// module.exports = {
+//   development: { ...sharedConfig },
+  
+//   test: { ...sharedConfig },
+  
+//   production: {
+//     client: 'pg',
+//     connection: process.env.DATABASE_URL,
+//     migrations: { directory: '../data/migrations' },
+//     seeds: { directory: '../data/seeds' },
+//     pool: {
+//       min: 2,
+//       max: 10,
+//     },
+//   },
+// };
+
+
 var dotenv = require('dotenv');
 dotenv.config({ path: '../.env' });
 
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.LOCAL_DATABASE,
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
     pool: {
@@ -22,7 +61,10 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
+    },
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
   },
