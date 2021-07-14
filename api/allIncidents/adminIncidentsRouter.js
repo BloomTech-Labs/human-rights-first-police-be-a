610a15d6eeb37c010700833b073d834056ef6322
abe-one <77995299+abe-one@users.noreply.github.com>
2021-07-14 17:20:26 -0500

    Build DELETE Endpoint

--- api/allIncidents/adminIncidentsRouter.js
+++ api/allIncidents/adminIncidentsRouter.js
@@ -6,11 +6,18 @@ const {
   authRequired,
   checkIncidentExists,
   validateIncident,
-} = require('../middleware'); //TODO add/build middleware
+} = require('../middleware');
+
+//TODO add/build middleware
+// TODO refractor error handling into single error handler
+// ? Admin gets direct error messages from database?
+// TODO document shape of objects coming and going
+
+router.use(authRequired);
 
 /**
  * @swagger
- * /incidents:
+ * /:
  *  GET:
  *    Summary: Path returning all incidents in reverse chronological order and filtered according to req.queries:
  *      {
@@ -30,8 +37,6 @@ const {
  *        description: Server response error
  */
 
-router.use(authRequired);
-
 router.get('/', validateQueries, async (req, res, next) => {
   const sanitizedQueries = req.sanitizedQueries;
 
@@ -44,8 +49,24 @@ router.get('/', validateQueries, async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /{incident_id}:
+ *  GET:
+ *    Summary: Path returning single incident by incident_id
+ *    tags:
+ *      - incidents
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      200:
+ *        description: Success ... returns incident object
+ *      500:
+ *        description: Server response error
+ */
+
 router.get('/:incident_id', checkIncidentExists, async (req, res, next) => {
-  const { id } = req.params.incident_id;
+  const id = req.params.incident_id;
 
   try {
     const incident = await Incidents.getIncidentById(id);
@@ -56,12 +77,28 @@ router.get('/:incident_id', checkIncidentExists, async (req, res, next) => {
   }
 });
 
+/**
+ * @swagger
+ * /{incident_id}:
+ *  PUT:
+ *    Summary: Path updating single incident by incident_id
+ *    tags:
+ *      - incidents
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      201:
+ *        description: Success ... returns updated incident object
+ *      500:
+ *        description: Server response error
+ */
+
 router.put(
-  '/incidents/:id',
+  '/:incident_id',
   checkIncidentExists,
   validateIncident,
   async (req, res) => {
-    const { id } = req.params;
+    const id = req.params.incident_id;
     const changes = req.sanitizedIncident;
     try {
       const updatedIncident = await Incidents.updateIncident(id, changes);
@@ -72,6 +109,22 @@ router.put(
   }
 );
 
+/**
+ * @swagger
+ * /{incident_id}:
+ *  POST:
+ *    Summary: Path posting new incident
+ *    tags:
+ *      - incidents
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      201:
+ *        description: Success ... returns new incident object
+ *      500:
+ *        description: Server response error
+ */
+
 router.post('/', validateIncident, async (req, res) => {
   try {
     const newIncident = await Incidents.createIncident(req.sanitizedIncident);
@@ -80,3 +133,32 @@ router.post('/', validateIncident, async (req, res) => {
     res.status(500).json({ message: 'Request Error' });
   }
 });
+
+/**
+ * @swagger
+ * /{incident_id}:
+ *  DELETE:
+ *    Summary: Path deleting incident by id
+ *    tags:
+ *      - incidents
+ *    produces:
+ *      - application/json
+ *    responses:
+ *      200:
+ *        description: Success ... returns new incident object
+ *      500:
+ *        description: Server response error
+ */
+
+router.delete('/:incident_id', checkIncidentExists, async (req, res, next) => {
+  const id = req.params.incident_id;
+
+  try {
+    const deletedIncident = await Incidents.delete(id);
+    res.status(200).json(deletedIncident);
+  } catch (error) {
+    res.status(500).json({ message: 'Request Error' });
+  }
+});
+
+module.exports = router;
