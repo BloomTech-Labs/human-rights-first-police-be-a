73a0c7176d3908ef6a37e6ea0a5ec3e09bcad18a
Francis Nguyen <francishtknguyen@gmail.com>
2021-07-14 18:51:17 -0500

    made suggested changes

--- data/migrations/20210713225521_updated-table.js
+++ data/migrations/20210713225521_updated-table.js
@@ -2,20 +2,20 @@ exports.up = function (knex) {
   return knex.schema
     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('incidents', (incidents) => {
-      incidents.increments('id');
-      incidents.date('date_created');
-      incidents.string('tweet_id');
-      incidents.string('user_name');
-      incidents.varchar('desc', 10000);
+      incidents.increments('incident_id');
+      incidents.date('date_created').notNullable();
+      incidents.string('tweet_id').notNullable();
+      incidents.string('user_name').notNullable();
+      incidents.varchar('desc', 10000).notNullable();
       incidents.string('city').defaultTo(null);
       incidents.string('state').defaultTo(null);
       incidents.float('lat').defaultTo(null);
       incidents.float('long').defaultTo(null);
       incidents.string('title').defaultTo(null);
-      incidents.string('force_rank');
-      incidents.string('status').defaultTo('pending');
+      incidents.string('force_rank').notNullable();
+      incidents.string('status').defaultTo('pending').notNullable();
       incidents.float('confidence');
-      incidents.string('tags');
+      incidents.string('tags').notNullable();
     })
     .createTable('profiles', function (table) {
       table.string('id').notNullable().unique().primary();
@@ -28,5 +28,8 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema
     .dropTableIfExists('incidents')
+    .dropTableIfExists('twitter_incidents') //To remove older db table
+    .dropTableIfExists('incidents_new') //To remove older db table
+    .dropTableIfExists('twitter_incidents_new') //To remove older db table
     .dropTableIfExists('profiles');
 };
