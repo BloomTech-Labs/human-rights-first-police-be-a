8042817bb02d20b751777f877ad79587b4d12780
Justin Peczenij <justinpeczenij@gmail.com>
2021-07-19 12:17:53 -0400

    Rename id to incident_id for compatability with new table

--- data/seeds/001-incidents.js
+++ data/seeds/001-incidents.js
@@ -6,7 +6,6 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('incidents').insert([
         {
-          id: 1,
           date_created: '2021-03-01T00:00:00.000Z',
           tweet_id: '1366291653267513344',
           user_name: 'shafiur',
@@ -23,7 +22,6 @@ exports.seed = function (knex) {
           tags: JSON.stringify(['police', 'kick', 'beat']),
         },
         {
-          id: 2,
           date_created: '2021-03-01T00:00:00.000Z',
           tweet_id: '1366320977223835648',
           user_name: 'campbellclaret',
@@ -40,7 +38,6 @@ exports.seed = function (knex) {
           tags: JSON.stringify(['police', 'lock', 'shut']),
         },
         {
-          id: 3,
           date_created: '2021-03-01T00:00:00.000Z',
           tweet_id: '1366398255588921351',
           user_name: 'walone4',
