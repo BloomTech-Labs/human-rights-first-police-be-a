6f210c827a27894dd94c3d96a024367c61e05497
abe-one <77995299+abe-one@users.noreply.github.com>
2021-07-20 18:27:02 -0500

    Refactor Legacy Incidents Seed To Match Final Table Schema

---
+++ data/seeds/01-legacy_incidents_seed.js
@@ -0,0 +1,47 @@
+const legacyIncidents = require('./legacyIncidents');
+
+const formatLegacyIncidents = (incidentsArray) => {
+  console.log(incidentsArray[0]);
+
+  const getTweetIdAndUsername = (sources) => {
+    const twitterLink = sources.find((src) => src.includes('twitter.com'));
+
+    if (!twitterLink) {
+      return ['', ''];
+    }
+    const idIndex = twitterLink.indexOf('status/') + 7;
+    const usernameIndex = twitterLink.indexOf('.com/') + 5;
+    const username = twitterLink.substring(usernameIndex, idIndex - 8);
+    const id = twitterLink.substring(idIndex);
+    return [id, username];
+  };
+
+  const formattedIncidents = incidentsArray.map((incident) => {
+    const [id, username] = getTweetIdAndUsername(incident.src);
+
+    incident.tags = incident.categories;
+    incident.date_created = incident.date;
+    incident.description =
+      incident.desc + '\nSources: ' + incident.src.join(' ');
+    incident.tweet_id = id;
+    incident.user_name = username;
+    incident.status = 'approved';
+    delete incident.id;
+    delete incident.src;
+    delete incident.date;
+    delete incident.desc;
+    delete incident.added_on;
+    delete incident.categories;
+    delete incident.incident_id;
+
+    return incident;
+  });
+
+  console.log(formattedIncidents[0]);
+
+  return formattedIncidents;
+};
+
+exports.seed = function (knex) {
+  return knex('incidents').insert(formatLegacyIncidents([legacyIncidents[0]]));
+};
