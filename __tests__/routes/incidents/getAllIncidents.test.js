a669ab2f2093b86cc7f75115e6d70896250837c5
Bruno Paula <devbrunopaula@gmail.com>
2021-08-10 14:02:58 -0400

    Fixes some typos and added a more descriptive describre block name.

--- __tests__/routes/incidents/getAllIncidents.test.js
+++ __tests__/routes/incidents/getAllIncidents.test.js
@@ -7,18 +7,18 @@ describe('Sanity Test', () => {
   });
 });
 
-describe('testt', () => {
+describe('[GET] Testing getIncidents EndPoint', () => {
   it('should fetch DB and return a 200 status', async () => {
     const res = await request(server).get('/incidents/getincidents');
     expect(res.status).toBe(200);
   });
 
-  it('should fecth DB and return an array with incidents', async () => {
+  it('Should fetch DB and return an array with incidents', async () => {
     const res = await request(server).get('/incidents/getincidents');
     expect(Array.isArray(res.body)).toBe(true);
   });
 
-  it('should fecth DB and return an incident with ID# 4', async () => {
+  it('Should fetch DB and return an incident with ID# 4', async () => {
     const res = await request(server).get('/incidents/getincidents');
 
     expect(res.body).toContainEqual(
