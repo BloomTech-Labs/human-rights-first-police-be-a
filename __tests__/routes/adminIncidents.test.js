74fac26eea4396ac9da62c201e70964d02b990c7
Ryder <ryderblue2@gmail.com>
2021-08-12 16:15:22 -0700

    Added more robust testing for Approved and Pending incidents

--- __tests__/routes/adminIncidents.test.js
+++ __tests__/routes/adminIncidents.test.js
@@ -20,6 +20,18 @@ describe('[GET] /dashboard/incidents', () => {
     const res = await request(server).get('/dashboard/incidents/');
     expect(res.status).toBe(200);
   });
+
+  it('returns only Pending incidents', async () => {
+    const res = await request(server).get('/dashboard/incidents/');
+    let pending = 0;
+    for (let i = 0; i < res.body.length; i++) {
+      if (res.body[i].status == 'pending') {
+        pending += 1;
+      }
+    }
+    console.log(pending);
+    expect(res.body.length).toBe(pending);
+  });
 });
 
 //Checks getAllApprovedIncidents inside Admin Router
@@ -27,5 +39,17 @@ describe('[GET] /dashboard/incidents/approved', () => {
   it('returns a status 200 OK', async () => {
     const res = await request(server).get('/dashboard/incidents/approved');
     expect(res.status).toBe(200);
+  });
+
+  it('returns only Approved incidents', async () => {
+    const res = await request(server).get('/dashboard/incidents/approved');
+    let approved = 0;
+    for (let i = 0; i < res.body.length; i++) {
+      if (res.body[i].status == 'approved') {
+        approved += 1;
+      }
+    }
+    console.log(approved);
+    expect(res.body.length).toBe(approved);
   });
 });
